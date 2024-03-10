a0da407dca9e8c08d6fc3ec19a21f034eaaa491f
creaffy <creaffy@gmail.com>
2024-03-10 14:42:38 +0100

    Add get_module()

--- README.md
+++ README.md
@@ -16,5 +16,7 @@ for (LDR_DATA_TABLE_ENTRY& mod : ldr::iterator()) {
 
 ```cpp
 // ldr::get_module does not care about lower/uppercase
-std::println("kernel32.dll -> {}", ldr::get_module("kernel32.dll")->DllBase);
+auto k32_base = ldr::get_module("kernel32.dll")->DllBase;
+std::println("kernel32.dll -> {}", k32_base);
+std::println("OpenProcess -> {}", ldr::get_export(k32_base, "OpenProcess"));
 ```
--- ldr.h
+++ ldr.h
@@ -7,7 +7,7 @@ namespace ldr {
     class iterator {
     public:
         iterator()
-            : _Entry(0) {
+            : _Entry(nullptr) {
         }
 
         iterator(LIST_ENTRY* Ptr)
@@ -55,20 +55,42 @@ namespace ldr {
         }
     };
 
+    // via ldr module list
     const LDR_DATA_TABLE_ENTRY* get_module(const std::string_view ModuleName) {
         std::wstring _LcTargetName(ModuleName.begin(), ModuleName.end());
         for (auto& e : _LcTargetName) {
             e = ::tolower(e);
         };
+
         for (auto it = iterator::begin(); it != iterator::end(); ++it) {
             std::wstring _LcThisName(it->FullDllName.Buffer);
             for (auto& e : _LcThisName) {
                 e = ::tolower(e);
             };
+
             std::wstring_view _LcThisFileName(_LcThisName.begin() + _LcThisName.find_last_of('\\') + 1, _LcThisName.end());
             if (_LcTargetName == _LcThisName || _LcTargetName == _LcThisFileName)
                 return it.get();
         }
+
+        return nullptr;
+    }
+
+    // via image headers
+    void* get_export(void* Module, const std::string_view ProcName) {
+        std::uintptr_t          _Module = reinterpret_cast<std::uintptr_t>(Module);
+        IMAGE_DOS_HEADER*       _DosHeader = reinterpret_cast<IMAGE_DOS_HEADER*>(Module);
+        IMAGE_NT_HEADERS*       _NtHeaders = reinterpret_cast<IMAGE_NT_HEADERS*>(_Module + _DosHeader->e_lfanew);
+        IMAGE_DATA_DIRECTORY*   _Directory = &_NtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+        IMAGE_EXPORT_DIRECTORY* _ExportTable = reinterpret_cast<IMAGE_EXPORT_DIRECTORY*>(_Module + _Directory->VirtualAddress);
+        std::uint32_t*          _NamesTable = reinterpret_cast<std::uint32_t*>(_Module + _ExportTable->AddressOfNames);
+        std::uint32_t*          _FuncsTable = reinterpret_cast<std::uint32_t*>(_Module + _ExportTable->AddressOfFunctions);
+
+        for (std::uint32_t i = 0; i < _ExportTable->NumberOfFunctions; i++) {
+            if (ProcName == reinterpret_cast<char*>(_Module + _NamesTable[i]))
+                return reinterpret_cast<void*>(_Module + _FuncsTable[i]);
+        }
+
         return nullptr;
     }
 }
